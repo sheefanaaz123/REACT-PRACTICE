7543eed9181c72c9b652ba678a9f46cd7d4e0e61
Sheefa Naaz <sheefanaaz6417@gmail.com>
2024-08-30 23:38:34 +0530

    Initial commit

---
+++ CHAPTER2/create-react-comp/src/App.jsx
@@ -0,0 +1,11 @@
+import KgButton from "./KgButton";
+
+function App() {
+  return (
+    <>
+      Heading <KgButton />
+    </>
+  );
+}
+
+export default App;
---
+++ CHAPTER2/create-react-comp/src/KgButton.jsx
@@ -0,0 +1,4 @@
+function KgButton() {
+  return <button>Hello</button>;
+}
+export default KgButton;
